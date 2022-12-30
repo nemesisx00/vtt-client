e872dcc12fb1dbde19998785b00fae2f9a4ad62f
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2022-12-29 21:25:33 -0500

    Update Connect.cs
    Tweaked positioning and size on the popup.

--- scripts/Connect.cs
+++ scripts/Connect.cs
@@ -32,7 +32,8 @@ public partial class Connect : Button
 			dialog.Title = "Connection Succeeded!";
 			dialog.DialogText = System.Text.Encoding.UTF8.GetString(body);
 			this.AddChild(dialog);
-			dialog.Position = new Vector2i(400, 320);
+			dialog.Size = new Vector2i(300, 50);
+			dialog.Position = new Vector2i(640 - 150, 360 - 25);
 			dialog.Popup();
 		}
 		else
