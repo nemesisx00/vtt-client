673fe5bc86ab7bdd31d28b44fb5d6540d2afa492
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2022-12-29 22:03:05 -0500

    Refined Popup Creation
    Abstracted the popup dialog creation into a method.

--- scripts/Connect.cs
+++ scripts/Connect.cs
@@ -5,6 +5,8 @@ public partial class Connect : Button
 {
 	private const string url = "http://127.0.0.1";
 	private const string port = "8080";
+	private const string DialogPopupName = "DialogPopup";
+	private const string PathToDialog = "%Connect/{0}";
 	
 	public override void _Ready()
 	{
@@ -28,15 +30,31 @@ public partial class Connect : Button
 		if(responseCode == 200)
 		{
 			GD.Print("Success!");
-			var dialog = new AcceptDialog();
-			dialog.Title = "Connection Succeeded!";
-			dialog.DialogText = System.Text.Encoding.UTF8.GetString(body);
-			this.AddChild(dialog);
-			dialog.Size = new Vector2i(300, 50);
-			dialog.Position = new Vector2i(640 - 150, 360 - 25);
-			dialog.Popup();
+			createDialogPopup("Connection Succeeded!", System.Text.Encoding.UTF8.GetString(body));
 		}
 		else
+		{
 			GD.Print("Fail!");
+			createDialogPopup("Connection Failed!", "Request timed out.");
+		}
+	}
+	
+	public void cleanUpDialog()
+	{
+		GetNode(String.Format(PathToDialog, DialogPopupName)).QueueFree();
+	}
+	
+	private void createDialogPopup(string title, string text)
+	{
+		var dialog = new AcceptDialog();
+		dialog.Title = title;
+		dialog.DialogText = text;
+		dialog.GetOkButton().Pressed += cleanUpDialog;
+		dialog.UniqueNameInOwner = true;
+		dialog.Name = DialogPopupName;
+		AddChild(dialog);
+		dialog.Size = new Vector2i(300, 50);
+		dialog.Position = new Vector2i(640 - 150, 360 - 25);
+		dialog.Popup();
 	}
 }
