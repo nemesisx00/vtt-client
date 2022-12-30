d47ec3a2af02784b22a89d82ba785bcd61290b5b
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2022-12-29 21:18:55 -0500

    Proof of Concept HTTPRequest
    Added a Connect.cs script for the Connect button which generates an HTTP request to the local loopback address, on port 8080, and on success displays the response body in a popup dialog.

---
+++ scripts/Connect.cs
@@ -0,0 +1,41 @@
+using Godot;
+using System;
+
+public partial class Connect : Button
+{
+	private const string url = "http://127.0.0.1";
+	private const string port = "8080";
+	
+	public override void _Ready()
+	{
+		Pressed += pressHandler;
+		GetNode<HTTPRequest>("%HttpConnect").RequestCompleted += doRequestComplete;
+	}
+	
+	public void pressHandler()
+	{
+		GetNode<HTTPRequest>("%HttpConnect").Request(
+			String.Format("{0}:{1}", url, port),
+			null,
+			true,
+			HTTPClient.Method.Get,
+			""
+		);
+	}
+	
+	public void doRequestComplete(long result, long responseCode, string[] headers, byte[] body)
+	{
+		if(responseCode == 200)
+		{
+			GD.Print("Success!");
+			var dialog = new AcceptDialog();
+			dialog.Title = "Connection Succeeded!";
+			dialog.DialogText = System.Text.Encoding.UTF8.GetString(body);
+			this.AddChild(dialog);
+			dialog.Position = new Vector2i(400, 320);
+			dialog.Popup();
+		}
+		else
+			GD.Print("Fail!");
+	}
+}
