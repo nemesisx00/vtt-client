4a8abb01403dd112111589aa5ede7edefa002ba2
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2022-12-31 01:00:54 -0500

    Refined Connect UI
    Added a real ugly popup to manually enter the IP/URL and port to which to connect. Mostly works but needs a lot of polish.

--- scripts/Connect.cs
+++ scripts/Connect.cs
@@ -3,58 +3,17 @@ using System;
 
 public partial class Connect : Button
 {
-	private const string url = "http://127.0.0.1";
-	private const string port = "8080";
-	private const string DialogPopupName = "DialogPopup";
-	private const string PathToDialog = "%Connect/{0}";
+	private const string ConnectionPanelPath = "%ConnectionPanel";
 	
 	public override void _Ready()
 	{
 		Pressed += pressHandler;
-		GetNode<HTTPRequest>("%HttpConnect").RequestCompleted += doRequestComplete;
 	}
 	
 	public void pressHandler()
 	{
-		GetNode<HTTPRequest>("%HttpConnect").Request(
-			String.Format("{0}:{1}", url, port),
-			null,
-			true,
-			HTTPClient.Method.Get,
-			""
-		);
-	}
-	
-	public void doRequestComplete(long result, long responseCode, string[] headers, byte[] body)
-	{
-		if(responseCode == 200)
-		{
-			GD.Print("Success!");
-			createDialogPopup("Connection Succeeded!", System.Text.Encoding.UTF8.GetString(body));
-		}
-		else
-		{
-			GD.Print("Fail!");
-			createDialogPopup("Connection Failed!", "Request timed out.");
-		}
-	}
-	
-	public void cleanUpDialog()
-	{
-		GetNode(String.Format(PathToDialog, DialogPopupName)).QueueFree();
-	}
-	
-	private void createDialogPopup(string title, string text)
-	{
-		var dialog = new AcceptDialog();
-		dialog.Title = title;
-		dialog.DialogText = text;
-		dialog.GetOkButton().Pressed += cleanUpDialog;
-		dialog.UniqueNameInOwner = true;
-		dialog.Name = DialogPopupName;
-		AddChild(dialog);
-		dialog.Size = new Vector2i(300, 50);
-		dialog.Position = new Vector2i(640 - 150, 360 - 25);
-		dialog.Popup();
+		var node = GetNode<Popup>(ConnectionPanelPath);
+		node.PopupCentered();
+		node.Size = new Vector2i(250, 110);
 	}
 }
---
+++ scripts/DoConnect.cs
@@ -0,0 +1,64 @@
+using Godot;
+using System;
+
+public partial class DoConnect : Button
+{
+	private const string ConnectionPanelPath = "%ConnectionPanel";
+	private const string ConnectUrlPath = "%ConnectUrl";
+	private const string ConnectPortPath = "%ConnectPort";
+	private const string DialogPopupName = "DialogPopup";
+	private const string PathToDialog = "%Connect/{0}";
+	
+	public override void _Ready()
+	{
+		Pressed += pressHandler;
+		GetNode<HTTPRequest>("%HttpConnect").RequestCompleted += doRequestComplete;
+	}
+	
+	public void pressHandler()
+	{
+		var url = GetNode<TextEdit>(ConnectUrlPath).Text;
+		var port = GetNode<TextEdit>(ConnectPortPath).Text;
+		
+		GetNode<HTTPRequest>("%HttpConnect").Request(
+			String.Format("{0}:{1}", url, port),
+			null,
+			true,
+			HTTPClient.Method.Get,
+			""
+		);
+	}
+	
+	public void doRequestComplete(long result, long responseCode, string[] headers, byte[] body)
+	{
+		if(responseCode == 200)
+		{
+			GD.Print("Success!");
+			GetNode<Popup>(ConnectionPanelPath).Hide();
+			createDialogPopup("Connection Succeeded!", System.Text.Encoding.UTF8.GetString(body));
+		}
+		else
+		{
+			GD.Print("Fail!");
+			createDialogPopup("Connection Failed!", "Request timed out.");
+		}
+	}
+	
+	public void cleanUpDialog()
+	{
+		GetNode(String.Format(PathToDialog, DialogPopupName)).QueueFree();
+	}
+	
+	private void createDialogPopup(string title, string text)
+	{
+		var dialog = new AcceptDialog();
+		dialog.Title = title;
+		dialog.DialogText = text;
+		dialog.GetOkButton().Pressed += cleanUpDialog;
+		dialog.UniqueNameInOwner = true;
+		dialog.Name = DialogPopupName;
+		AddChild(dialog);
+		dialog.PopupCentered();
+		dialog.Size = new Vector2i(300, 50);
+	}
+}
